2ae82a35fb0c2ac0dc6616e07ba19749024da6f7
S Anandaraj Srinivasan <SAnandaraj.Srinivasan@Honeywell.com>
2021-07-18 18:24:56 +0530

    Add project files.

---
+++ BaseAPI/Controllers/WeatherForecastController.cs
@@ -0,0 +1,85 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace BaseAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class WeatherForecastController : ControllerBase
+    {
+        private static readonly string[] Summaries = new[]
+        {
+            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+        };
+
+        private readonly ILogger<WeatherForecastController> _logger;
+        private readonly IHttpClientFactory _clientFactory;
+
+        public WeatherForecastController(ILogger<WeatherForecastController> logger, IHttpClientFactory httpClientFactory)
+        {
+            _logger = logger;
+            _clientFactory = httpClientFactory;
+        }
+
+        [HttpGet]
+        public IEnumerable<WeatherForecast> Get()
+        {
+            _logger.LogInformation("Get Method Called...");
+
+            var rng = new Random();
+            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
+            {
+                Date = DateTime.Now.AddDays(index),
+                TemperatureC = rng.Next(-20, 55),
+                Summary = Summaries[rng.Next(Summaries.Length)]
+            })
+            .ToArray();
+        }
+
+        [HttpGet]
+        [Route("Test")]
+        public async Task<IActionResult> GetData()
+        
+        {
+            var requestEndPoint = "https://localhost:44387/weatherforecast/Test";
+
+            _logger.LogInformation($"Get Data From Micro Service 1 {requestEndPoint} - Started");
+
+            try
+            {
+                // Content from BBC One: Dr. Who website (©BBC)
+                var request = new HttpRequestMessage(HttpMethod.Get,
+                   requestEndPoint);
+
+                var client = _clientFactory.CreateClient();
+
+                Request.Headers.TryGetValue("CorrelationId", out var correlationId).ToString();
+
+                var id = correlationId;
+
+                var response = await client.SendAsync(request);
+
+                if (response.IsSuccessStatusCode)
+                {                    
+                    var data = await response.Content.ReadAsStringAsync();
+
+                    return Ok(data);
+                }
+
+                return BadRequest(response.StatusCode);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError($"Error from GetData - {requestEndPoint}: {ex.Message}");
+                throw;
+            }
+
+        }
+    }
+}
---
+++ BaseAPI/Startup.cs
@@ -0,0 +1,74 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Serilog;
+using Serilog.Events;
+using BaseAPI.Middlewares;
+using System.IO;
+
+namespace BaseAPI
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+
+            //logging 
+
+            Enum.TryParse(Configuration.GetValue<string>("Logging:LogLevel:Default"), out LogEventLevel logLevel);
+
+            Log.Logger = new LoggerConfiguration()
+                .WriteTo.RollingFile(Path.Combine(Environment.CurrentDirectory, "log-{Date}.txt"), logLevel,
+                    outputTemplate: "{Timestamp:yyyy-MM-dd HH:mm:ss.fff zzz} [{Level:u3}] {CorrelationId} {Message:lj}{NewLine}{Exception}")
+                .CreateLogger();
+
+
+            //Configure Http client
+
+            services.AddHttpClient();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env, ILoggerFactory loggerFactory)
+        {
+            loggerFactory.AddSerilog();
+
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+
+            app.UseMiddleware<SerilogMiddleware>();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
